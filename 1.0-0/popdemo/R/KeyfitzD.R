e2dcd29a026f44e549a95e547df10c5dfe809cae
iainmstott <iainmstott@gmail.com>
2016-09-28 13:34:20 +0200

    Add files via upload

---
+++ 1.0-0/popdemo/R/KeyfitzD.R
@@ -0,0 +1,59 @@
+################################################################################
+#' Calculate Keyfitz's delta
+#'
+#' @description
+#' Calculate Keyfitz's delta for a population matrix projection model.
+#'
+#' @param A a square, irreducible, non-negative numeric matrix of any dimension.
+#'
+#' @param vector a numeric vector or one-column matrix describing the age/stage 
+#' distribution used to calculate the distance.
+#'
+#' @details 
+#' Keyfitz's delta is the sum of the differences between the stable demographic 
+#' vector (the dominant right eigenvector of \code{A}) and the demographic 
+#' distribution vector of the population (given by \code{vector}). 
+#' \code{KeyfitzD} will not work for reducible matrices and returns a 
+#' warning for imprimitive matrices (although will not function for imprimitive 
+#' matrices with nonzero imaginary components in the dominant eigenpair).
+#'
+#' @return 
+#' Keyfitz's delta.
+#'
+#' @references
+#' Keyfitz (1968) Introduction to the Mathematics of Populations. Addison-Wesley.\cr
+#' Stott et al. (2010) Ecol. Lett., 14, 959-970.
+#'
+#' @family DistanceMeasures
+#'
+#' @examples
+#'   # Create a 3x3 PPM
+#'   ( A <- matrix(c(0,1,2,0.5,0.1,0,0,0.6,0.6), byrow=TRUE, ncol=3) )
+#'
+#'   # Create an initial stage structure
+#'   ( initial <- c(1,3,2) )
+#'   
+#'   # Calculate Keyfitz's delta
+#'   KeyfitzD(A, vector=initial)
+#'
+#' @concept 
+#' distance vector state space
+#'
+#' @export
+#'
+KeyfitzD <-
+function(A,vector){
+if(any(length(dim(A))!=2,dim(A)[1]!=dim(A)[2])) stop("A must be a square matrix")
+if(!isIrreducible(A)) stop("Matrix A is reducible")
+if(!isPrimitive(A)) warning("Matrix A is imprimitive")
+reigs<-eigen(A)
+lmax<-which.max(Re(reigs$values))
+w<-as.matrix(reigs$vectors[,lmax])
+if(max(Im(w))>0) stop("Dominant right eigenvector contains nonzero imaginary components")
+w<-abs(Re(w))
+w<-w/sum(w)
+vector<-as.matrix(vector/sum(vector))
+delta<-0.5*norm(vector-w)
+return(delta)
+}
+
---
+++ 1.0-0/popdemo/R/sens.R
@@ -0,0 +1,67 @@
+################################################################################
+#' Calculate sensitivity matrix
+#'
+#' @description
+#' Calculate the sensitivity matrix for a population matrix projection model 
+#' using eigenvectors.
+#'
+#' @param A a square, non-negative numeric matrix of any dimension
+#'
+#' @param eval the eigenvalue to evaluate. Default is \code{eval="max"}, which 
+#' evaluates the dominant eigenvalue (the eigenvalue with largest REAL value: 
+#' for imprimitive or reducible matrices this may not be the first eigenvalue). 
+#' Otherwise, specifying e.g. \code{eval=2} will evaluate sensitivity of the 
+#' eigenvalue with second-largest modulus.
+#'
+#' @param all (optional) if \code{FALSE}, then only sensitivity values for 
+#' observed transitions (nonzero entries in \code{A}) are returned.
+#'
+#' @details 
+#' \code{sens} uses the eigenvectors of \code{A} to calculate the sensitivity 
+#' matrix of the specified eigenvalue, see section 9.1 in Caswell (2001). 
+#' Same method as \code{\link[popbio]{sensitivity}} but can also evaluate
+#' subdominant eigenvalues.
+#'
+#' @return 
+#' A numeric (real or complex) matrix of equal dimension to \code{A}.
+#'
+#' @references
+#' Caswell (2001) Matrix Population Models 2nd ed. Sinauer.
+#'
+#' @family PerturbationAnalyses
+#'
+#' @examples
+#'   # Create a 3x3 PPM
+#'   ( A <- matrix(c(0,1,2,0.5,0.1,0,0,0.6,0.6), byrow=TRUE, ncol=3) )
+#'
+#'   # Calculate sensitivities of dominant eigenvalue
+#'   sens(A)
+#*
+#'   # Calculate sensitivities of first subdominant eigenvalue,
+#'   # only for observed transitions
+#'   sens(A, eval=2, all=FALSE)
+#'
+#' @concept 
+#' perturbation sensitivity elasticity 
+#'
+#' @export
+#'
+sens <-
+function(A,eval="max",all=FALSE){
+if(any(length(dim(A))!=2,dim(A)[1]!=dim(A)[2])) stop("A must be a square matrix")
+order<-dim(A)[1]
+reigs<-eigen(A)
+leigs<-eigen(t(A))
+if(eval=="max"){
+    val<-which.max(Re(reigs$values))
+}
+else{
+    val<-eval
+}
+w<-as.matrix(reigs$vectors[,val])
+v<-as.matrix(leigs$vectors[,val])
+S<-(Conj(v)%*%t(w))/as.vector(Conj(t(v))%*%w)
+if(!all) S[A==0]<-0
+if(max(Im(S))>0) return(S) else(return(Re(S)))
+}
+
