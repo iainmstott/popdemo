f3296e949df0bc6a6280c14c9a045f2df2403674
iainmstott <iainmstott@gmail.com>
2018-03-15 19:40:35 +0100

    final changes to prepare for new package version

--- 1.2-1Dev/popdemoDev/R/popdemo-onAttach.R
+++ 1.2-1Dev/popdemoDev/R/popdemo-onAttach.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname){
 packageStartupMessage(
-"Welcome to popdemo! This is version 1.2-1
+"Welcome to popdemo! This is version 1.3-0
 Use ?popdemo for an intro, or browseVignettes('popdemo') for vignettes
 Citation for popdemo is here: doi.org/10.1111/j.2041-210X.2012.00222.x
 Development and legacy versions are here: github.com/iainmstott/popdemo"
--- 1.2-1Dev/popdemoDev/R/popdemo-package.R
+++ 1.2-1Dev/popdemoDev/R/popdemo-package.R
@@ -2,31 +2,32 @@
 #' Provides tools for demographic modelling using projection matrices
 #'
 #' @description
-#' \code{popdemo} provides a set of tools designed for modelling population 
-#' demography using population projection matrix (PPM) models. Specific foci are 
-#' indices of transient dynamics, and the use of control theory.  However, 
-#' \code{popdemo} may also be useful for other implementations of MPMs, 
-#' or matrix models in a more general sense. \cr\cr
-#' The package is designed to build on those tools already available in 
-#' \code{popbio}.\cr\cr
+#' \code{popdemo} provides tools for modelling populations and demography using 
+#' matrix projection models (MPMs), with deterministic and stochastic model 
+#' implementations. These tools include population projection, indices of short- 
+#' and long-term population size and growth, perturbation analysis, convergence 
+#' to stability or stationarity, and diagnostic and manipulation tools.\cr\cr
 #' Selected functions (see bottom of page for a link to the full index):\cr\cr
 #' Functions useful for working with matrices:\cr
 #' \code{popdemo} contains a number of tools to ease working with matrices
-#' (and specifically PPMs) in \R.
+#' (and specifically MPMs) in \R.
 #' \code{\link{Matlab2R}} allows coding of matrices in a Matlab style, which
 #' also facilitates import of multiple matrices simultaneously if comma-seperated
-#' files are used to import dataframes.  Its analogue, \code{\link{R2Matlab}},
+#' files are used to import dataframes. Its analogue, \code{\link{R2Matlab}},
 #' converts \R matrices to Matlab-style strings, for easier export.\cr
 #' \code{\link{isPrimitive}}, \code{\link{isIrreducible}} and
 #' \code{\link{isErgodic}} facilitate diagnosis of matrix properties
 #' pertaining to ergodicity.\cr\cr
 #' Population projection:\cr
 #' \code{popdemo} provides a simple means of projecting and plotting PPM models.
-#' \code{\link{project}} will project population dynamics and a plotting method
-#' is available.\cr\cr
+#' \code{\link{project}} provides a means to project and plot population 
+#' dynamics of both deterministic and stochastic models. Many methods are available
+#' for working with population projections: see \code{\link{Projection-class}} and 
+#' \code{\link{Projection-plots}}\cr\cr
+#' Asymptotic population dynamics:\cr
+#' The \code{\link{eigs}} function provides a simple means to calculate asymptotic 
+#' population dynamics using matrix eigenvalues.
 #' Indices of transient density:\cr
-#' Transient dynamics are important to study in PPM models, and \code{popdemo}
-#' provides the means to work with transient indices of PPM models.
 #' \code{\link{reac}} measures immediate transient density of a population 
 #' (within the first time step). 
 #' \code{\link{maxamp}} and \code{\link{maxatt}} are near-term indices that 
@@ -37,18 +38,21 @@
 #' can be calculated using specific population structures, as well as bounds on
 #' population size.\cr\cr
 #' Perturbation analysis:\cr
-#' Perturbation analysis is a key part of population studies. \code{popdemo} provides
-#' methods for nonlinear perturbation analysis of both asymptotic dynamics, using \code{\link{tfa_lambda}} and
-#' \code{\link{tfam_lambda}}, and transient dynamics, using \code{\link{tfa_inertia}} and
-#' \code{\link{tfam_inertia}}.  These all have associated plotting methods linked to them: see
-#' \code{\link{plot.tfa}} and \code{\link{plot.tfam}}).  Sensitivity analyses are also available
-#' using transfer function methods: see \code{\link{tfs_lambda}}, \code{\link{tfsm_lambda}}, 
-#' \code{\link{tfs_inertia}} and \code{\link{tfsm_inertia}}.  (Traditional perturbation
-#' analyses are also available: \code{\link{sens}}, \code{\link{elas}}).\cr\cr
+#' Methods for linear perturbation (sensitivity and elasticity) analysis of asymptotic dynamics 
+#' are available through the \code{\link{sens}}, \code{\link{tfs_lambda}} and \code{\link{tfsm_lambda}} 
+#' functions. Elasticity analysis is also available using the \code{\link{elas}} function. 
+#' Sensitivity analysis of transient dynamics is available using the \code{\link{tfs_inertia}} 
+#' and \code{\link{tfsm_inertia}} functions.\cr
+#' Methods for nonlinear perturbation (transfer function) analysis of asymptotic 
+#' dynamics is achieved using \code{\link{tfa_lambda}} and \code{\link{tfam_lambda}}, 
+#' whilst transfer function analysis of transient dynamics is available with 
+#' \code{\link{tfa_inertia}} and \code{\link{tfam_inertia}}. These all have 
+#' associated plotting methods linked to them: see \code{\link{plot.tfa}} and 
+#' \code{\link{plot.tfam}}).\cr\cr
 #' Indices of convergence:\cr
 #' Information on the convergence of populations to stable state can be useful, and
 #' \code{popdemo} provides several means of analysing convergence.
-#' \code{dr} measures the damping ratio, and there are several distance measures available
+#' \code{\link{dr}} measures the damping ratio, and there are several distance measures available
 #' (see \code{\link{KeyfitzD}}, \code{\link{projectionD}} and 
 #' \code{\link{CohenD}}).  There is also a means of calculating convergence time
 #' through simulation: \code{\link{convt}}.\cr
